5db9c505ed3624c0e11a9f674909f7fa848e3fe6
Vaughn Dice <donvice@gmail.com>
2015-08-28 09:38:47 -0700

    add functionality and spec for band and venue validation of name

--- spec/band_spec.rb
+++ spec/band_spec.rb
@@ -2,4 +2,5 @@ require('spec_helper')
 
 describe(Band) do
   it { should have_and_belong_to_many(:venues) }
+  it { should validate_presence_of(:name) }
 end
--- spec/venue_spec.rb
+++ spec/venue_spec.rb
@@ -2,4 +2,5 @@ require('spec_helper')
 
 describe(Venue) do
   it { should have_and_belong_to_many(:bands) }
+  it { should validate_presence_of(:name) }
 end
