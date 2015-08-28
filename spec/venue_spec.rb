a62905bc97a6b5a300bb636f5d9ff8532e86b93d
Vaughn Dice <donvice@gmail.com>
2015-08-28 09:55:26 -0700

    add callback to band for capitalizing words in venue, pass spec

--- spec/venue_spec.rb
+++ spec/venue_spec.rb
@@ -3,4 +3,9 @@ require('spec_helper')
 describe(Venue) do
   it { should have_and_belong_to_many(:bands) }
   it { should validate_presence_of(:name) }
+
+  it('should be saved with capitalized words no matter how they are entered') do
+    lowercase_venue = Venue.create({:name => 'cervante\'s masterpiece ballroom'})
+    expect(lowercase_venue.name()).to(eq('Cervante\'s Masterpiece Ballroom'))
+  end
 end
