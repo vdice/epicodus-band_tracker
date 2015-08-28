e48ee76ccb71f7936c0839fec17893c276961b1c
Vaughn Dice <donvice@gmail.com>
2015-08-28 09:48:16 -0700

    add callback to band for capitalizing words in band name, pass spec

--- lib/band.rb
+++ lib/band.rb
@@ -1,4 +1,14 @@
 class Band < ActiveRecord::Base
   has_and_belongs_to_many(:venues)
   validates(:name, {:presence => true})
+  before_save(:capitalize_name)
+
+private
+  define_method(:capitalize_name) do
+    capitalized_words = []
+    self.name().split(" ").each() do |word|
+      capitalized_words.push(word.capitalize())
+    end
+    self.name=capitalized_words.join(" ")
+  end
 end
--- spec/band_spec.rb
+++ spec/band_spec.rb
@@ -3,4 +3,9 @@ require('spec_helper')
 describe(Band) do
   it { should have_and_belong_to_many(:venues) }
   it { should validate_presence_of(:name) }
+
+  it('should be saved with capitalized words no matter how they are entered') do
+    lowercase_band = Band.create({:name => 'the sonics'})
+    expect(lowercase_band.name()).to(eq('The Sonics'))
+  end
 end
